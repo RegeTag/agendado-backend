f2ed1840c2eef8053721b63fa87c66be47cb0ab3
Rodrigo Tavares <rodrigotvrs70@hotmail.com>
2021-06-30 02:30:33 -0300

    bad request error handle added

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import Express from "express"
+import 'express-async-errors'
 import router from "./routes"
 import "./database"
+import errorHandleMiddleware from "./middlewares/errorHandleMiddleware"
 
 const app = Express()
 
 app.use(Express.json())
 
 app.use(router)
+
+app.use(errorHandleMiddleware)
 
 export default app
