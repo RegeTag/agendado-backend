1edb2ab3bb0f710ddb8e596020ad706c8d12f501
Rodrigo Tavares <rodrigotvrs70@hotmail.com>
2021-07-06 16:29:37 -0300

    routes changes

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,28 @@
 import { Router } from 'express'
-import CreateReminderController from './controllers/CreateReminderController'
+import ensureAuthMiddleware from './middlewares/ensureAuthMiddleware'
 import CreateUserController from './controllers/CreateUserController'
+import DeleteUserController from './controllers/DeleteUserController'
+import CreateTokenController from './controllers/CreateTokenController'
+import CreateReminderController from './controllers/CreateReminderController'
+import GetUserRemindersController from './controllers/GetUserRemindersController'
 
 const router = Router()
 
-router.post("/reminders", CreateReminderController.handle)
 
-router.post("/users", CreateUserController.handle)
+// ==================== Reminder routes ==========================
 
+router.post("/reminders", ensureAuthMiddleware, CreateReminderController.handle)
+
+router.get("/users/reminders", ensureAuthMiddleware, GetUserRemindersController.handle)
+
+// ==================== User Routes ==============================
+
+router.post("/users/signup", CreateUserController.handle)
+
+router.post("/users/signin", CreateTokenController.handle)
+
+router.delete("/users", ensureAuthMiddleware, DeleteUserController.handle)
+
+// ==============================================================
 
 export default router
